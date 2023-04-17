bd52d8bad9ab96433407bf00ccd3fc910ae2fd10
xhwhust <32415101+xhwhust@users.noreply.github.com>
2023-04-17 22:16:18 +0800

    iterator design (#1)

---
+++ src/iterator/Book.java
@@ -0,0 +1,18 @@
+package iterator;
+
+/**
+ * @author xhw
+ * @version 1.0
+ * Create by 2023/4/17 21:55
+ */
+public class Book {
+    private String name ;
+
+    public Book(String name){
+        this.name = name ;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
